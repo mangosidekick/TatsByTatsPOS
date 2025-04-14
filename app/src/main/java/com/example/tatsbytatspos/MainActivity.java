55a4ff3f5040fb355d168d387637cfa232ad3b8f
mangosidekick <pokeyp0kers3@gmail.com>
2025-04-14 19:05:39 +0800

    nav bar changes (no edge to edge)

--- app/src/main/java/com/example/tatsbytatspos/MainActivity.java
+++ app/src/main/java/com/example/tatsbytatspos/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
         //NAV BAR STUFF RAAAAAAGHHH
